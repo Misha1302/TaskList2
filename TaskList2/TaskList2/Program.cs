905d0e0ed328beb6d063dea9ceb56d514395cc50
Михаил <misha13022008@gmail.com>
2023-09-10 13:19:39 +0300

    non-working changes

--- TaskList2/TaskList2/Program.cs
+++ TaskList2/TaskList2/Program.cs
@@ -1,3 +1,33 @@
+using Npgsql;
+
+var conn = new NpgsqlConnection("Host=localhost;Port=5432;Database=testDb;Username=postgres;Password=qwerty");
+
+conn.Open();
+
+// Passing PostGre SQL Function Name
+var command = new NpgsqlCommand("EXE GetEmployeePrintInfo", conn);
+
+// Execute the query and obtain a result set
+var reader = command.ExecuteReader();
+
+// Reading from the database rows
+var listOfManager = new List<string>();
+
+while (reader.Read())
+{
+    var wsManager =
+        reader["WSManager"].ToString() ??
+        throw new InvalidOperationException(); // Remember Type Casting is required here it has to be according to database column data type
+    listOfManager.Add(wsManager);
+}
+
+reader.Close();
+
+command.Dispose();
+conn.Close();
+
+return;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
